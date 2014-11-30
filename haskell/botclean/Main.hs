2430d4b4d70a0e6c115239901977750646cd0d48
Zach Moazeni <zach.moazeni@gmail.com>
2014-11-30 09:37:57 -0500

    Convert explicit state passing to Reader Monad
    Having fun learning the conversion

--- haskell/botclean/Main.hs
+++ haskell/botclean/Main.hs
@@ -1,13 +1,17 @@
+-- solution for https://www.hackerrank.com/challenges/botclean
+--   and https://www.hackerrank.com/challenges/botcleanr
 module Main where
 
 import Data.List
 import Data.Maybe
+import Control.Monad.Trans.Reader
 
-type Position      = (Int, Int)
-type Entry         = (Position, Char)
-type EntryWithCost = (Int, Entry)
-type Matrix        = [Entry]
-data Action        = GoUp | GoDown | GoRight | GoLeft | Clean
+type Position = (Int, Int)
+type Entry    = (Position, Char)
+type Matrix   = [Entry]
+data Action   = GoUp | GoDown | GoRight | GoLeft | Clean
+data Env      = Env { envPosition   :: Position,
+                      envMatrix     :: Matrix }
 
 instance Show Action where
   show dir = case dir of
@@ -17,35 +21,58 @@ instance Show Action where
     GoLeft  -> "LEFT"
     Clean   -> "CLEAN"
 
+
+dirtyValue :: Char
+dirtyValue = 'd'
+
+maxIndex :: Int
+maxIndex = 4
+
 main :: IO ()
 main = do
   input <- getContents
-  let (position, matrix) = getPositionAndMatrix input
-      nextAction = determineNextAction position matrix
+  let environment = getPositionAndMatrix input
+      nextAction = runReader determineNextAction environment
   putStrLn (show nextAction)
 
-findClosestEntry :: Position -> Matrix -> Entry
-findClosestEntry (x, y) matrix = snd (head entriesWithCost)
+findDirtyEntries :: Reader Env Matrix
+findDirtyEntries = do
+  Env {envMatrix=matrix} <- ask
+  return $ filter (\(_, v) -> v == dirtyValue) matrix
+
+findClosestEntry :: Reader Env Entry
+findClosestEntry = do
+  Env {envPosition=position} <- ask
+  dirtyEntries <- findDirtyEntries
+  let (closestEntry:_) = entriesWithCost (calculateCost position) dirtyEntries
+  return $ snd closestEntry
+
   where
-    dirtyEntries = filter (\(_, v) -> v == dirtyValue) matrix
-    entriesWithCost = sortBy compareCost $ map calculateCost dirtyEntries
-    calculateCost entry@((x', y'), _) = let cost = (abs $ x - x') + (abs $ y - y')
-                                                 in (cost, entry)
+    entriesWithCost mapper = sortBy compareCost . map mapper
+    calculateCost (x, y) entry@((x', y'), _) = let cost = (abs $ x - x') + (abs $ y - y')
+                                        in (cost, entry)
     compareCost (cost1, _) (cost2, _) = cost1 `compare` cost2
 
+currentValue :: Reader Env Char
+currentValue = do
+  Env {envMatrix=matrix, envPosition=position} <- ask
+  let currentEntry = fromJust $ find (\(pos, _) -> pos == position) matrix
+      (_, currentValue') = currentEntry
+  return currentValue'
 
-determineNextAction :: Position -> Matrix -> Action
-determineNextAction position matrix = nextAction value
-  where
-    value = let currentEntry = fromJust $ find (\(pos, _) -> pos == position) matrix
-                (_, currentValue) = currentEntry in currentValue
+closestPosition :: Reader Env Position
+closestPosition = do
+  (closestPosition', _) <- findClosestEntry
+  return closestPosition'
 
-    closestPosition = let (closestPosition', _) = findClosestEntry position matrix
-                      in closestPosition'
-
-    nextAction v
-      | v == dirtyValue = Clean
-      | otherwise       = originToDestination position closestPosition
+determineNextAction :: Reader Env Action
+determineNextAction = do
+  value <- currentValue
+  Env {envPosition=position} <- ask
+  closestPosition' <- closestPosition
+  if value == dirtyValue
+    then return Clean
+    else return (originToDestination position closestPosition')
 
 originToDestination :: Position -> Position -> Action
 originToDestination origin@(x, y) destination@(x', y')
@@ -57,22 +84,16 @@ originToDestination origin@(x, y) destination@(x', y')
     moveHorizontal = x /= x'
     moveVertical   = y /= y'
 
-dirtyValue :: Char
-dirtyValue = 'd'
-
-maxIndex :: Int
-maxIndex = 4
-
-getPositionAndMatrix :: String -> (Position, Matrix)
+getPositionAndMatrix :: String -> Env
 getPositionAndMatrix input =
-  let (firstLine:rest) = lines input
-      position = getPosition firstLine
-      matrix = buildMatrix rest
-  in (position, matrix)
+  let (positionLine:matrixLines) = lines input
+      position   = parseTwoInts positionLine
+      matrix     = buildMatrix matrixLines
+  in Env position matrix
 
-  where getPosition line = let ints = map read (words line) :: [Int]
-                               (y:x:_) = ints
-                           in (x, y)
+  where parseTwoInts line = let ints = map read (words line) :: [Int]
+                                (y:x:_) = ints
+                            in (x, y)
 
 buildMatrix :: [String] -> Matrix
 buildMatrix input = concatMap withXYIndex (withIndex input)
