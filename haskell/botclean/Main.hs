62a493d0bbd4a13caa039d01b901a38258b3c955
Zach Moazeni <zach.moazeni@gmail.com>
2014-11-29 20:43:15 -0500

    Optimizes the bot even more by finding the closest entry
    Instead of moving horizontal and then moving down when necessary, this
    calculates the nearest entry every time.

--- haskell/botclean/Main.hs
+++ haskell/botclean/Main.hs
@@ -3,14 +3,15 @@ module Main where
 import Data.List
 import Data.Maybe
 
-type Position = (Int, Int)
-type Entry = (Position, Char)
-type Matrix = [Entry]
-
-data Action = GoDown | GoRight | GoLeft | Clean
+type Position      = (Int, Int)
+type Entry         = (Position, Char)
+type EntryWithCost = (Int, Entry)
+type Matrix        = [Entry]
+data Action        = GoUp | GoDown | GoRight | GoLeft | Clean
 
 instance Show Action where
   show dir = case dir of
+    GoUp    -> "UP"
     GoDown  -> "DOWN"
     GoRight -> "RIGHT"
     GoLeft  -> "LEFT"
@@ -21,35 +22,40 @@ main = do
   input <- getContents
   let (position, matrix) = getPositionAndMatrix input
       nextAction = determineNextAction position matrix
-  --print position
-  --print matrix
   putStrLn (show nextAction)
 
+findClosestEntry :: Position -> Matrix -> Entry
+findClosestEntry (x, y) matrix = snd (head entriesWithCost)
+  where
+    dirtyEntries = filter (\(_, v) -> v == dirtyValue) matrix
+    entriesWithCost = sortBy compareCost $ map calculateCost dirtyEntries
+    calculateCost entry@((x', y'), _) = let cost = (abs $ x - x') + (abs $ y - y')
+                                                 in (cost, entry)
+    compareCost (cost1, _) (cost2, _) = cost1 `compare` cost2
+
+
 determineNextAction :: Position -> Matrix -> Action
-determineNextAction position@(x, y) matrix = nextAction value
+determineNextAction position matrix = nextAction value
   where
     value = let currentEntry = fromJust $ find (\(pos, _) -> pos == position) matrix
                 (_, currentValue) = currentEntry in currentValue
 
-    canOptimize = not $ any (\entry ->
-        dirtyCellOnCurrentRow position entry || willMissDirtyCellOnNextRow position entry
-      ) matrix
+    closestPosition = let (closestPosition', _) = findClosestEntry position matrix
+                      in closestPosition'
 
     nextAction v
-      | v == dirtyValue         = Clean
-      | canOptimize             = GoDown
-      | x == maxIndex && even y = GoDown
-      | x == 0 && odd y         = GoDown
-      | even y                  = GoRight
-      | odd y                   = GoLeft
-      | otherwise = error $ "not sure what to do with matrix: " ++ (show matrix) ++ " position: " ++ (show position) ++ " value: " ++ (show v)
+      | v == dirtyValue = Clean
+      | otherwise       = originToDestination position closestPosition
 
-dirtyCellOnCurrentRow :: Position -> Entry -> Bool
-dirtyCellOnCurrentRow (_, y) ((_, y'), value) = y == y' && value == dirtyValue
+originToDestination :: Position -> Position -> Action
+originToDestination origin@(x, y) destination@(x', y')
+  | moveHorizontal = if x' < x then GoLeft else GoRight
+  | moveVertical   = if y' < y then GoUp   else GoDown
+  | otherwise      = error $ "can't find direction from: " ++ (show origin) ++ " to: " ++ (show destination)
 
-willMissDirtyCellOnNextRow :: Position -> Entry -> Bool
-willMissDirtyCellOnNextRow (x, y) ((x', y'), value) = otherDirtyCells && (y + 1) == y' && value == dirtyValue
-  where otherDirtyCells = if odd y' then x' < x else x' > x
+  where
+    moveHorizontal = x /= x'
+    moveVertical   = y /= y'
 
 dirtyValue :: Char
 dirtyValue = 'd'
