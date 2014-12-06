a921fde0483f18cf16c363eba337e97ff30f8c45
Zach Moazeni <zach.moazeni@gmail.com>
2014-12-06 10:51:41 -0500

    Very naive solver to bruteforce the best path
    This doesn't work because it takes forever to calculate input-huge

---
+++ haskell/botclean_large/SolveAll.hs
@@ -0,0 +1,119 @@
+-- solution for https://www.hackerrank.com/challenges/botcleanlarge
+--
+-- Brute force for all possibilities
+module Main where
+
+import Data.List
+import Data.Maybe
+import Control.Monad.Trans.Reader
+
+type Position      = (Int, Int)
+newtype Dimensions = Dimensions (Int, Int) -- keep it separate from Position
+type Entry         = (Position, Char)
+type Matrix        = [Entry]
+data Action        = GoUp | GoDown | GoRight | GoLeft | Clean
+data Env = Env { envPosition   :: Position,
+                 envMatrix     :: Matrix,
+                 envDimensions :: Dimensions }
+
+instance Show Action where
+  show dir = case dir of
+    GoUp    -> "UP"
+    GoDown  -> "DOWN"
+    GoRight -> "RIGHT"
+    GoLeft  -> "LEFT"
+    Clean   -> "CLEAN"
+
+dirtyValue :: Char
+dirtyValue = 'd'
+
+maxIndex :: Int
+maxIndex = 4
+
+main :: IO ()
+main = do
+  input <- getContents
+  let environment = getPositionAndMatrix input
+      totalCost = runReader calculateTotalCost environment
+      --nextAction = runReader determineNextAction environment
+  --putStrLn (show nextAction)
+  --print totalCost
+  print $ take 20 $ map (\((cost, _), _) -> cost) totalCost
+
+calculateTotalCost :: Reader Env [((Int, Maybe Entry), Matrix)]
+calculateTotalCost = do
+  Env {envPosition=position, envMatrix=matrix} <- ask
+  let dirtyEntries = filter (\(_, v) -> v == dirtyValue) matrix
+      allPaths = permutations dirtyEntries
+      allPathsWithCost = map (\path -> mapAccumL costWithPath (0, Nothing) path) allPaths
+      sortedPaths = sortBy compareCost allPathsWithCost
+  return sortedPaths
+
+  where
+    costWithPath (cost, Nothing) entry = ((cost, Just entry), entry)
+    costWithPath (cost, Just lastEntry) entry = ((cost + (calculateCost lastEntry entry), Just entry), entry)
+
+    calculateCost ((x, y), _) ((x', y'), _) = (abs $ x - x') + (abs $ y - y')
+
+    compareCost ((cost, _), _) ((cost', _), _) = cost `compare` cost'
+
+determineNextAction :: Reader Env Action
+determineNextAction = do
+  value <- currentValue
+  Env {envPosition=position} <- ask
+  closestPosition' <- closestPosition
+  let action = if value == dirtyValue
+                 then Clean
+                 else originToDestination position closestPosition'
+  return action
+
+currentValue :: Reader Env Char
+currentValue = do
+  Env {envMatrix=matrix, envPosition=position} <- ask
+  let currentEntry = fromJust $ find (\(pos, _) -> pos == position) matrix
+      (_, currentValue') = currentEntry
+  return currentValue'
+
+closestPosition :: Reader Env Position
+closestPosition = do
+  Env {envMatrix=matrix, envPosition=position} <- ask
+  let dirtyEntries = filter (\(_, v) -> v == dirtyValue) matrix
+      (_, closestEntry) = head $ entriesWithCost (calculateCost position) dirtyEntries
+      (closestPosition', _) = closestEntry
+  return closestPosition'
+
+  where
+    entriesWithCost mapper = sortBy compareCost . map mapper
+    calculateCost (x, y) entry@((x', y'), _) = let cost = (abs $ x - x') + (abs $ y - y')
+                                               in (cost, entry)
+    compareCost (cost1, _) (cost2, _) = cost1 `compare` cost2
+
+originToDestination :: Position -> Position -> Action
+originToDestination origin@(x, y) destination@(x', y')
+  | moveHorizontal = if x' < x then GoLeft else GoRight
+  | moveVertical   = if y' < y then GoUp   else GoDown
+  | otherwise      = error $ "can't find direction from: " ++ (show origin) ++ " to: " ++ (show destination)
+
+  where
+    moveHorizontal = x /= x'
+    moveVertical   = y /= y'
+
+getPositionAndMatrix :: String -> Env
+getPositionAndMatrix input =
+  let (positionLine:dimensionLine:matrixLines) = lines input
+      position   = parseTwoInts positionLine
+      dimensions = Dimensions (parseTwoInts dimensionLine)
+      matrix     = buildMatrix matrixLines
+  in Env position matrix dimensions
+
+  where parseTwoInts line = let ints = map read (words line) :: [Int]
+                                (y:x:_) = ints
+                            in (x, y)
+
+buildMatrix :: [String] -> Matrix
+buildMatrix input = concatMap withXYIndex (withIndex input)
+  where
+    withIndex = zip [0..]
+
+    withXYIndex :: (Int, String) -> [(Position, Char)]
+    withXYIndex (yIndex, line) = map (\(xIndex, char) -> ((xIndex, yIndex), char)) (withIndex line)
